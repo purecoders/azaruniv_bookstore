d1358fa90b7447d246bb7dd4c68f925c7b300bf6
unknown <mohsenfarjami340@gmail.com>
2019-05-10 17:46:14 +0430

    change home page text align

--- resources/views/site/welcome.blade.php
+++ resources/views/site/welcome.blade.php
@@ -2,45 +2,45 @@
 @section('content')
     {{--<div class="container-fluid py-3 slide-background">--}}
 
-        {{--<div class="slide-container m-auto">--}}
-            {{--<div id="carousel" class="swiper-container carousel" dir="rtl">--}}
-                {{--<div class="swiper-wrapper">--}}
-                    {{--@foreach($sliders as $slider)--}}
-                        {{--<div class="swiper-slide"><img class="w-100" src="{{asset($slider->image_path)}}"/></div>--}}
-                    {{--@endforeach--}}
-                {{--</div>--}}
-                {{--<!-- Add Pagination -->--}}
-                {{--<div class="swiper-pagination"></div>--}}
-                {{--<!-- Add Arrows -->--}}
-                {{--<div class="swiper-button-next"></div>--}}
-                {{--<div class="swiper-button-prev"></div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
+    {{--<div class="slide-container m-auto">--}}
+    {{--<div id="carousel" class="swiper-container carousel" dir="rtl">--}}
+    {{--<div class="swiper-wrapper">--}}
+    {{--@foreach($sliders as $slider)--}}
+    {{--<div class="swiper-slide"><img class="w-100" src="{{asset($slider->image_path)}}"/></div>--}}
+    {{--@endforeach--}}
+    {{--</div>--}}
+    {{--<!-- Add Pagination -->--}}
+    {{--<div class="swiper-pagination"></div>--}}
+    {{--<!-- Add Arrows -->--}}
+    {{--<div class="swiper-button-next"></div>--}}
+    {{--<div class="swiper-button-prev"></div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
     {{--</div>--}}
 
     {{--<div class="intro m-0 p-4 mt-3">--}}
-        {{--<div class="row p-3 text-center">--}}
-            {{--<div class="col-lg-4 p-2      ">--}}
-                {{--<div class="intro-card d-flex flex-column align-items-center justify-content-center rtl text-center p-4">--}}
-                    {{--<h4 class="text-main-color">همه ی کتاب های انتشارات دانشگاه شهید مدنی آذربایجان در دسترس شما</h4>--}}
-                    {{--<i class="fa fa-book   position-absolute "></i>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--<div class="col-lg-4 p-2 ">--}}
-                {{--<div class="intro-card d-flex flex-column align-items-center justify-content-center rtl text-center p-4">--}}
-                    {{--<h4 class="text-main-color">ارسال رایگان تمامی سفارشات</h4>--}}
-                    {{--<i class="fa fa-shopping-cart   position-absolute "></i>--}}
-                {{--</div>--}}
+    {{--<div class="row p-3 text-center">--}}
+    {{--<div class="col-lg-4 p-2      ">--}}
+    {{--<div class="intro-card d-flex flex-column align-items-center justify-content-center rtl text-center p-4">--}}
+    {{--<h4 class="text-main-color">همه ی کتاب های انتشارات دانشگاه شهید مدنی آذربایجان در دسترس شما</h4>--}}
+    {{--<i class="fa fa-book   position-absolute "></i>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="col-lg-4 p-2 ">--}}
+    {{--<div class="intro-card d-flex flex-column align-items-center justify-content-center rtl text-center p-4">--}}
+    {{--<h4 class="text-main-color">ارسال رایگان تمامی سفارشات</h4>--}}
+    {{--<i class="fa fa-shopping-cart   position-absolute "></i>--}}
+    {{--</div>--}}
 
-            {{--</div>--}}
-            {{--<div class="col-lg-4 p-2   ">--}}
-                {{--<div class="intro-card d-flex flex-column align-items-center justify-content-center rtl text-center p-4">--}}
-                    {{--<h4 class="text-main-color">جهت کسب اطلاعات بیشتر با شماره یا ایمیل انتشارات تماس بگیرید</h4>--}}
-                    {{--<i class="fa fa-info   position-absolute "></i>--}}
-                {{--</div>--}}
-            {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="col-lg-4 p-2   ">--}}
+    {{--<div class="intro-card d-flex flex-column align-items-center justify-content-center rtl text-center p-4">--}}
+    {{--<h4 class="text-main-color">جهت کسب اطلاعات بیشتر با شماره یا ایمیل انتشارات تماس بگیرید</h4>--}}
+    {{--<i class="fa fa-info   position-absolute "></i>--}}
+    {{--</div>--}}
+    {{--</div>--}}
 
-        {{--</div>--}}
+    {{--</div>--}}
     {{--</div>--}}
 
     <div class=" bg-light books-container p-3">
@@ -55,7 +55,7 @@
                 </a>
             @endforeach
         </div>
-        <div id="allBooks" class="d-flex flex-row flex-wrap m-0 p-1 p-sm-3">
+        <div id="allBooks" class="d-flex flex-row flex-wrap m-0 p-1 p-sm-3 justify-content-center">
             @foreach($books as $book)
                 <a href="{{route('detail', $book->id)}}" class="d-block text-center book-link">
                     <div class="book-container d-flex flex-column align-items-center m-3" style="min-height: 300px !important;">
